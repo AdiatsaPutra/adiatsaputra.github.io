e405832478f3851bc97f1be1e459cfeae1f5f226
Adiatsa Putra <santikosetyo77@gmail.com>
2023-10-13 21:33:13 +0700

    google fomnt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: "/portfolio/",
-    buildAssetsDir: "assets",
-  },
+  // app: {
+  //   baseURL: "/",
+  //   buildAssetsDir: "assets",
+  // },
   devtools: { enabled: true },
 
   modules: [
@@ -24,5 +24,6 @@ export default defineNuxtConfig({
       Poppins: [400, 700],
     },
     display: "swap",
+    download: false,
   },
 });
